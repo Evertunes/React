0a5b5d45679f038407225d19885544e119f14245
Everton Alves <alveseverton023@gmail.com>
2023-05-10 00:05:33 -0300

    modif

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//cria o root do react
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//chama a função render
+//App é o componente renderizado
+root.render(
+    <App />
+);
